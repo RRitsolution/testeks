8123bb63092c5170d3860b9f6a0ba5dc3bb3cba7
RRITsolutions <rritsolutions2316@gmail.com>
2025-12-03 07:38:06 +0100

    Update helminstall.sh

--- helminstall.sh
+++ helminstall.sh
@@ -4,6 +4,6 @@ curl -o iam_policy.json   https://raw.githubusercontent.com/kubernetes-sigs/aws-
 aws iam create-policy --policy-name AWSLoadBalancerControllerIAMPolicy --policy-document file://iam_policy.json
 eksctl create iamserviceaccount --cluster production-eks --namespace kube-system --name aws-load-balancer-controller --attach-policy-arn arn:aws:iam::631844602603:policy/AWSLoadBalancerControllerIAMPolicy --override-existing-serviceaccounts --region us-east-1 --approve
 sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 777 get_helm.sh
+sudo chmod 777 get_helm.sh
 bash get_helm.sh
 helm -version
